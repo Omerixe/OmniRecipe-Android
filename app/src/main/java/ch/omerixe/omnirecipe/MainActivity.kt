dff7dd4a5f5bbcbf9645ce1a99c25d5be6f02581
Jasmin <jasmin@omerixe.ch>
2024-04-06 16:33:54 +0200

    Initial Commit

---
+++ app/src/main/java/ch/omerixe/omnirecipe/MainActivity.kt
@@ -0,0 +1,46 @@
+package ch.omerixe.omnirecipe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ch.omerixe.omnirecipe.ui.theme.OmniRecipeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            OmniRecipeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Omni Recipe")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    OmniRecipeTheme {
+        Greeting("Android")
+    }
+}
